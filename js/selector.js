38adc27ab4f46fcfbdf5feb0a9a487686eae079e
goforu <99goforu@gmail.com>
2016-06-23 18:05:09 +0800

    third commit

--- js/selector.js
+++ js/selector.js
@@ -1,39 +1,36 @@
 /*!
- * Common Selector v0.0.1
+ * Table Selector v0.0.1
  *
  * var selector = new Selector(
- *      id:selectorId, //selector显示位置（必填）
- *      context:context, //selector所在上下文（必填）
- *      index:0, //关键字段所在列，若不设置则返回选中项时返回全部字段
- *      title:title, //selector标题
- *      hidable:true, //是否允许未选中时隐藏，默认为true
- *      actions:[ //添加操作按钮
+ *      id:selectorId, //selector id
+ *      index:0, //index of key data
+ *      title:title, //selector title
+ *      hidable:true, //hide without selection; default:true
+ *      actions:[ //add button
  *          {
- *              name:name,
- *              action:function(p1,p2){},
- *              param:[p1,p2]
+ *              name:name,//button name
+ *              action:function(){},//button action
  *          }
  *      ],
  *      table:{
- *          id:tableId,//操作的表单id//(必填)
- *          data:"data", //表单提取数据的tr属性名，默认为data。若数据有多个用","隔开
- *          splitter:"," //data的分隔符，默认为","
+ *          id:tableId,//table id
+ *          data:"data", //attribute set on tr or td
+ *          splitter:"," //split multiple data
  *      },
- *      listener:{ //监听某对象方法
- *          context:obj,//被监听对象
- *          target:fn, //被监听方法
- *          callback: function(p1,p2){},//方法触发时响应
- *          param:[p1,p2],//callback传入参数
- *          async:index// 被监听方法中，回调参数的位置（用于异步方法的监听）
+ *      listener:{ //trigger when a certain function called
+ *          context:obj,//
+ *          method:fn, //listened function
+ *          action: function(){},//call after listened function called
+ *          async:index// position of the argument in an async function
  *      }
  * );
  */
 +function ($) {
     this.Selector = function (jsn) {
-        this.template = '<div class="table-selector"><div class="table-selector-title"></div> <span class="selector-delete-all">×</span><ul class="table-selector-item"></ul> <div class="selector-action-container"></div></div>';
+        this._template = '<div class="table-selector"><div class="table-selector-title"></div><ul class="table-selector-item"></ul> <div class="selector-action-container"></div></div>';
         this.selectedItems = [];
         this.index = jsn.index;
-        this.actions = jsn.actions;
+        this.actions = jsn.actions || [];
         this.table = jsn.table || {};
         this.table.splitter = jsn.table.splitter || ",";
         this.table.data = jsn.table.data || "data";
@@ -41,22 +38,21 @@
         //this.id = jsn.id;
         this.node = $('#' + jsn.id);
         this.title = jsn.title;
-        this.context = jsn.context;
-        this.listener = jsn.listener || [];//监听方法触发
-        this.hidable = jsn.hidable == undefined ? true : jsn.hidable;//无选中时，是否可以隐藏
-        this.prop = jsn.style;
+        //this.context = jsn.context;
+        this.listeners = jsn.listeners || [];
+        this.hidable = jsn.hidable == undefined ? true : jsn.hidable;
+        this.style = jsn.style;
     };
 
     Selector.prototype = {
-        //初始化
         _init: function (obj) {
             var _t = this;
             this.node = obj || this.node;
             if (!this.node || !this.node[0] instanceof HTMLElement) throw new Error('object is undefined or is not an HTMLElement');
             _t.table.id && $('#' + _t.table.id).addClass('selectable').addClass('selectable-' + _t.table.type);
-            _t.node.html(_t.template);
+            _t.node.html(_t._template);
         },
-        //清空数据
+        //clear selected items
         clear: function () {
             this.selectedItems = [];
             this.refreshRows();
@@ -72,29 +68,29 @@
                 _t.node.hide();
             }
         },
-        //猴子补丁，监听操作
+        //monkey patch
         bindListener: function (funObj) {
             var _t = this;
             if (funObj != undefined) {
-                Object.prototype.toString.call(funObj) === '[object Array]' ? _t.listener.concat(funObj) : _t.listener.push(funObj);
+                Object.prototype.toString.call(funObj) === '[object Array]' ? _t.listeners.concat(funObj) : _t.listeners.push(funObj);
             }
-            if (_t.listener.length) {
-                $.each(_t.listener, function (i, p) {
+            if (_t.listeners.length) {
+                $.each(_t.listeners, function (i, p) {
                     if (!p.original) {
-                        p.original = p.context[p.target];
+                        p.original = p.context[p.method];
                     }
+                    p.context[p.method] = function () {
                     var async = p.async != undefined && typeof arguments[p.async] === "function";
-                    p.context[p.target] = function () {
                         if (async) {
                             var tempFun = arguments[p.async];
                             arguments[p.async] = function () {
-                                var r = tempFun.apply(this, arguments);
-                                p.listen.apply(p.context, p.param);
+                                var r = tempFun.apply(this, arguments);// won't work if callback function rely on context
+                                p.action();
                                 return r;
                             }
                         }
-                        var result = p.original.apply(this, arguments);
-                        !async && p.listen.apply(p.context, p.param);
+                        var result = p.original.apply(p.context, arguments);
+                        !async && p.action();
                         return result;
                     }
                 });
@@ -103,28 +99,25 @@
         },
         unbindListener: function (context/*object*/, fn/*string*/) {
             var _t = this;
-            $.each(_t.listener, function (i, p) {
-                if (context[fn] === p.context[p.target]) {
+            $.each(_t.listeners, function (i, p) {
+                if (context[fn] === p.context[p.method]) {
                     context[fn] = p.original;
-                    _t.listener.splice(i, 1);
+                    _t.listeners.splice(i, 1);
                 }
             });
             return this;
         },
-        unbindAllListener: function () {
+        unbindAllListeners: function () {
             var _t = this;
-            $.each(_t.listener, function (i, p) {
-                p.context[p.target] = p.original
+            $.each(_t.listeners, function (i, p) {
+                p.context[p.method] = p.original
             });
-            _t.listener = [];
+            _t.listeners = [];
             return this;
         },
-        //绑定click事件
+        //bind click events on table
         _bindEvent: function () {
             var _t = this;
-            _t.node.find('.table-selector .selector-delete-all').on('click', function () {
-                _t.clear();
-            });
             if (_t.table.id) {
                 $('#' + _t.table.id).unbind().on('click', _t.table.type + '[' + _t.table.data + "]", function (e) {
                     if ($(e.target).is('a, input')) return;
@@ -137,7 +130,7 @@
 
             }
         },
-        //删除、更新等操作后，重新渲染表格
+        //refresh after data changes
         refreshRows: function (/*optional*/option, /*optional*/item) {
             var _t = this;
             if (_t.table.id) {
@@ -151,6 +144,7 @@
                             }
                             break;
                         default:
+                            $(dom).hasClass('selected') && $(dom).removeClass('selected');
                             for (var p in list) {
                                 var data = $(dom).attr(_t.table.data).split(_t.table.splitter);
                                 if (_t._isDataEqual(list[p], data)) {
@@ -158,13 +152,12 @@
                                     list.splice(p, 1);
                                 }
                             }
-                            $(dom).hasClass('selected') && $(dom).removeClass('selected');
                     }
                 });
             }
             return this;
         },
-        //判断数据是否相等
+
         _isDataEqual: function (data1, data2) {
             for (var i = 0; i < data1.length; ++i) {
                 if (data1[i] !== data2[i]) {
@@ -173,17 +166,17 @@
             }
             return true;
         },
-        //渲染
+
         _render: function () {
             var _t = this;
-            _t.prop && _t.node.css($.extend(_t.prop, {overflow: "auto"}));
-            $('.table-selector #selector-action-container').empty();
+            _t.style && _t.node.css($.extend(_t.style, {overflow: "auto"}));
+            _t.node.find('.table-selector .selector-action-container').empty();
             _t._renderActionButton();
-            _t.title && $('.table-selector .table-selector-title').text(_t.title);
+            _t.title && _t.node.find('.table-selector .table-selector-title').text(_t.title);
             _t.refreshRows();
             _t._showOrHide();
         },
-        //显示控件
+
         show: function (obj) {
             this._init(obj);
             this._render();
@@ -191,28 +184,28 @@
             this._bindEvent();
             return this;
         },
-        //
+
         _renderActionButton: function () {
             var _t = this;
             _t.node.find('.table-selector .selector-action-container').empty();
             $.each(_t.actions, function (i, p) {
                 var actionButton = '<input type="button" value="' + p.name + '"/>';
                 $(actionButton).appendTo(_t.node.find('.table-selector .selector-action-container')).on('click', function () {
-                    p.action.apply(p.context || _t.context, p.param);
+                    p.action.apply(p.context || _t);
                     //p.action();
                 });
             });
         },
-        //添加操作按钮
+        //add button
         addActionButton: function (obj) {
             var _t = this;
             if (obj != undefined) {
-                Object.prototype.toString.call(obj) === '[object Array]' ? _t.actions.concat(obj) : _t.actions.push(obj);
+                Object.prototype.toString.call(obj) === '[object Array]' ? _t.actions = _t.actions.concat(obj) : _t.actions.push(obj);
             }
             _t._renderActionButton();
             return this;
         },
-        //添加选中项
+
         pushItem: function (item) {
             var _t = this;
             if (typeof item === 'string') item = [item];
@@ -228,7 +221,7 @@
             _t._showOrHide();
             return this;
         },
-        //删除选中项
+
         removeItem: function (item) {
             var _t = this;
             //var index = _t.index;
@@ -242,7 +235,7 @@
             });
             return this;
         },
-        //获得所有选中项
+
         getSelectedItems: function () {
             var _t = this;
             if (_t.index != null) {
@@ -254,6 +247,12 @@
                 return rst;
             }
             return this.selectedItems;
+        },
+        destroy: function(){
+            var _t = this;
+            _t.unbindAllListeners();
+            _t.table.id && $('#' + _t.table.id).removeClass('selectable').removeClass('selectable-' + _t.table.type);
+            _t.node.empty();
         }
     };
 }(jQuery);
